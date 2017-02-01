95187807f0b0ece2f41a9eff74d15fe2fee50fc2
Yamashta Daisuke <yamasdais@gmail.com>
2017-02-02 01:12:31 +0900

    added using pacage.install.201702$

--- R/DoDtSci_2.1.R
+++ R/DoDtSci_2.1.R
@@ -7,9 +7,16 @@ data1$agecat <- cut(data1$Age, c(-Inf, 0, 18, 24, 34, 44, 54, 64, Inf))
                                         # summary
 summary(data1)
 
-install.packages("doBy")
+#install.packages("doBy")
 library("doBy")
 siterange <- function(x) {
     c(length(x), min(x), mean(x), max(x))
 }
 summaryBy(Age~agecat, data = data1, FUN = siterange)
+
+summaryBy(Gender+Signed_In+Impressions,Clicks~agecat,
+          data = data1)
+
+install.packages("ggplot2")
+library(ggpplot2)
+
