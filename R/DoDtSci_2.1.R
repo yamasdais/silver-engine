3a40663883c19da94dc28768bfb9bdebea1ca48f
Yamashta Daisuke <yamasdais@gmail.com>
2017-02-01 12:55:57 +0900

    added DoDtSci_2.1.R

---
+++ R/DoDtSci_2.1.R
@@ -0,0 +1,15 @@
+data1 <- read.csv(url("http://stat.columbia.edu/~rachel/datasets/nyt1.csv"))
+
+                                        # split by age
+head(data1)
+data1$agecat <- cut(data1$Age, c(-Inf, 0, 18, 24, 34, 44, 54, 64, Inf))
+
+                                        # summary
+summary(data1)
+
+install.packages("doBy")
+library("doBy")
+siterange <- function(x) {
+    c(length(x), min(x), mean(x), max(x))
+}
+summaryBy(Age~agecat, data = data1, FUN = siterange)
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 祝 初めての GitHub
 
-機械学習の学習
+# 機械学習の学習
+
+# Map
+
+R/ R read by source() command.
+doc/ text
