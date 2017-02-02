4a72c31e71f69269c316876a5f90205c0dae3101
Yamashta Daisuke <yamasdais@gmail.com>
2017-02-02 12:50:09 +0900

    wrote DoDtSci_2.1.R

--- R/DoDtSci_2.1.R
+++ R/DoDtSci_2.1.R
@@ -8,15 +8,47 @@ data1$agecat <- cut(data1$Age, c(-Inf, 0, 18, 24, 34, 44, 54, 64, Inf))
 summary(data1)
 
 #install.packages("doBy")
-library("doBy")
+library(doBy)
 siterange <- function(x) {
     c(length(x), min(x), mean(x), max(x))
 }
 summaryBy(Age~agecat, data = data1, FUN = siterange)
 
-summaryBy(Gender+Signed_In+Impressions,Clicks~agecat,
+summaryBy(Gender+Signed_In+Impressions+Clicks~agecat,
           data = data1)
 
-install.packages("ggplot2")
-library(ggpplot2)
+                                        # plot
 
+#install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(data1, aes(x = Impressions, fill = agecat))
+       +geom_histogram(binwidth = 1)
+ggplot(data1, aes(x = agecat, y = Impressions, fill = agecat))
+       +geom_boxplot()
+
+                                        # click through
+data1$hasimps <- cut(data1$Impressions, c(-Inf, 0, Inf))
+summaryBy(Clicks~hasimps, data = data1, FUN = siterange)
+ggplot(subset(data1, Impressions > 0),
+       aes(x = Clicks/Impressions, colour = agecat)) + geom_density()
+ggplot(subset(data1, Clicks > 0),
+       aes(x = Clicks/Impressions, colour = agecat)) + geom_density()
+ggplot(subset(data1, Clicks > 0),
+       aes(x = agecat, y = Clicks, fill = agecat)) + geom_boxplot()
+ggplot(subset(data1, Clicks > 0),
+       aes(x = Clicks, colour = agecat)) + geom_density()
+
+                                        # making category
+data1$scode[data1$Impressions == 0] <- "NoImps"
+data1$scode[data1$Impressions > 0] <- "Imps"
+data1$scode[data1$Clicks > 0] <- "Clicks"
+
+data1$scode <- factor(data1$scode)
+head(data1)
+
+clen = function(x) {
+    c(length(x))
+}
+etable <- summaryBy(Impressions~scode+Gender+agecat,
+                    data = data1, FUN = clen)
--- README.md
+++ README.md
@@ -2,7 +2,19 @@
 
 # 機械学習の学習
 
-# Map
+# 言語
+## R
+R のライブラリは install.packages("...") で、インストールできる。
+個人用ライブラリは、~/R/../3.3/ に入る。
+使うときは、library(...)
+ggplot2 は ubuntu のパッケージで用意されてる。
+探すときは apt-cache search r-cran を使うといい。
 
-R/ R read by source() command.
+ライブラリパスを調べたければ、
+ > .libPaths()
+を使うといい。
+
+# ディレクトリ構成
+
+R/ R
 doc/ text
