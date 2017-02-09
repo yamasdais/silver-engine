14f6f2eca58ab96b170f7512dd3fa51b76cf9901
Yamashta Daisuke <yamasdais@gmail.com>
2017-02-09 13:05:43 +0900

    tried for expresstion

--- scala/hello.scala
+++ scala/hello.scala
@@ -2,3 +2,8 @@ val you: Option[String] =
   if (args.length == 0) None
   else Some(args(0))
 println("Hello, " + you.getOrElse("World") + "!")
+for (
+  targ <- args
+) {
+  println("Hello, " + targ + "!")
+}
